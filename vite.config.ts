3069894eef430c405574417cef43d5cb844cadff
Daniel Mark <danielmark.uc@gmail.com>
2025-04-09 23:52:42 +0530

    fix: removed vitest

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from "vitest/config";
+import { defineConfig } from "vite";
 import path, { resolve } from "path";
 import { peerDependencies, dependencies } from "./package.json";
 import react from "@vitejs/plugin-react";
@@ -14,11 +14,6 @@ export default defineConfig({
       outputDir: "dist",
     }),
   ],
-  test: {
-    globals: true,
-    environment: "jsdom",
-    setupFiles: "./vitest.setup.ts",
-  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src"),
